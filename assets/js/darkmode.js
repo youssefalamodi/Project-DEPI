df021d367ef2fd3f496984936908d89aaede701a
ElecNinja <yostroke90@gmail.com>
2024-09-21 16:57:47 +0300

    Adjusted Theme Button For Small Screens

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -1,6 +1,9 @@
 let darkmode = localStorage.getItem('darkmode');
 const themeSwitch = document.getElementById('theme-switch');
 
+const darkswitch = document.getElementById('darkswitch');
+const lightswitch = document.getElementById('lightswitch');
+
 const enableDarkMode = () => {
     document.body.classList.add('darkmode')
     localStorage.setItem('darkmode', 'active')
@@ -22,6 +25,21 @@ else{
 
 
 themeSwitch.addEventListener("click", () => {
+    darkmode = localStorage.getItem('darkmode');
+    // darkmode !== "active" ? enableDarkMode() : disableDarkMode();
+    if(darkmode !== "active")
+    {
+        enableDarkMode();
+        document.getElementById("amazon-logo").src = "./assets/images/logo/Untitled.png";
+    }
+    else
+    {
+    disableDarkMode();
+    document.getElementById("amazon-logo").src = "./assets/images/logo/Amazon_logo.svg";
+    }
+    
+})
+darkswitch.addEventListener("click", () => {
     darkmode = localStorage.getItem('darkmode');
     // darkmode !== "active" ? enableDarkMode() : disableDarkMode();
     if(darkmode !== "active")
