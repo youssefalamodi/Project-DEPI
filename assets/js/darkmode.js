39fb31f3ef283f2711f0b2528a40a61ba2681573
Mohamed Mahmoud <moh2661978@gmail.com>
2024-09-21 21:11:58 +0300

    new

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -1,56 +1,50 @@
-let darkmode = localStorage.getItem('darkmode');
-const themeSwitch = document.getElementById('theme-switch');
+let darkmode = localStorage.getItem("darkmode");
+const themeSwitch = document.getElementById("theme-switch");
 
-const darkswitch = document.getElementById('darkswitch');
-const lightswitch = document.getElementById('lightswitch');
+const darkswitch = document.getElementById("darkswitch");
+const lightswitch = document.getElementById("lightswitch");
 
 const enableDarkMode = () => {
-    document.body.classList.add('darkmode')
-    localStorage.setItem('darkmode', 'active')
-}
+  document.body.classList.add("darkmode");
+  localStorage.setItem("darkmode", "active");
+};
 
 const disableDarkMode = () => {
-    document.body.classList.remove('darkmode')
-    localStorage.setItem('darkmode', null)
+  document.body.classList.remove("darkmode");
+  localStorage.setItem("darkmode", null);
+};
+
+if (darkmode === "active") {
+  enableDarkMode();
+  document.getElementById("amazon-logo").src =
+    "./assets/images/logo/Untitled.png";
+} else {
+  document.getElementById("amazon-logo").src =
+    "./assets/images/logo/Amazon_logo.svg";
 }
-
-if (darkmode === "active"){ enableDarkMode();
- document.getElementById("amazon-logo").src = "./assets/images/logo/Untitled.png";
-}
-else{
-    document.getElementById("amazon-logo").src = "./assets/images/logo/Amazon_logo.svg";
-}
-;
-
-
-
 themeSwitch.addEventListener("click", () => {
-    darkmode = localStorage.getItem('darkmode');
-    // darkmode !== "active" ? enableDarkMode() : disableDarkMode();
-    if(darkmode !== "active")
-    {
-        enableDarkMode();
-        document.getElementById("amazon-logo").src = "./assets/images/logo/Untitled.png";
-    }
-    else
-    {
+  darkmode = localStorage.getItem("darkmode");
+  // darkmode !== "active" ? enableDarkMode() : disableDarkMode();
+  if (darkmode !== "active") {
+    enableDarkMode();
+    document.getElementById("amazon-logo").src =
+      "./assets/images/logo/Untitled.png";
+  } else {
     disableDarkMode();
-    document.getElementById("amazon-logo").src = "./assets/images/logo/Amazon_logo.svg";
-    }
-    
-})
+    document.getElementById("amazon-logo").src =
+      "./assets/images/logo/Amazon_logo.svg";
+  }
+});
 darkswitch.addEventListener("click", () => {
-    darkmode = localStorage.getItem('darkmode');
-    // darkmode !== "active" ? enableDarkMode() : disableDarkMode();
-    if(darkmode !== "active")
-    {
-        enableDarkMode();
-        document.getElementById("amazon-logo").src = "./assets/images/logo/Untitled.png";
-    }
-    else
-    {
+  darkmode = localStorage.getItem("darkmode");
+  // darkmode !== "active" ? enableDarkMode() : disableDarkMode();
+  if (darkmode !== "active") {
+    enableDarkMode();
+    document.getElementById("amazon-logo").src =
+      "./assets/images/logo/Untitled.png";
+  } else {
     disableDarkMode();
-    document.getElementById("amazon-logo").src = "./assets/images/logo/Amazon_logo.svg";
-    }
-    
-})
+    document.getElementById("amazon-logo").src =
+      "./assets/images/logo/Amazon_logo.svg";
+  }
+});
